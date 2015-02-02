62448b31978dea2d7d1b4f7cb90c61c3de34de94
Roberts, Jerry <Jerry.Roberts@e-hps.com>
2015-02-02 11:16:39 -0500

    Initial Commit

---
+++ Heartland-Tokenization/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Heartland-Tokenization
+//
+//  Created by Roberts, Jerry on 2/2/15.
+//  Copyright (c) 2015 Heartland Payment Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
