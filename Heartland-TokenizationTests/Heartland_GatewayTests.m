46fa12c7898d677a3df9b17e642dd7d3279bfa36
Roberts, Jerry <Jerry.Roberts@e-hps.com>
2015-09-08 19:23:31 -0400

    Added empty unit test file for gateway examples

---
+++ Heartland-TokenizationTests/Heartland_GatewayTests.m
@@ -0,0 +1,18 @@
+//
+//  Heartland_GatewayTests.m
+//  Heartland-Tokenization
+//
+//  Created by Roberts, Jerry on 9/8/15.
+//  Copyright (c) 2015 Heartland Payment Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Heartland_GatewayTests : XCTestCase
+
+@end
+
+@implementation Heartland_GatewayTests
+
+@end
